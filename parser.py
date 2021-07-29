82a650bdd9dd175e1e0317dfeca9ffe201bd9b39
Карина Ханова <hanovakm@gmail.com>
2021-07-29 17:16:49 +0300

    mini df filecreation

--- parser.py
+++ parser.py
@@ -1,13 +1,11 @@
 import requests
 import json
-import os
-import tqdm
+import aiohttp
+import asyncio
 from io import StringIO
 from html.parser import HTMLParser
 import pandas as pd
 import time
-import queue
-import threading
 
 PATH = "files"
 
@@ -33,14 +31,6 @@ def strip_tags(html):
     return s.get_data()
 
 
-class MultiThread(threading.Thread):
-    def __init__(self):
-        super().__init__()
-
-    def run(self):
-        pass
-
-
 class Parser():
     def __init__(self, text, area, per_page):
         self.text = "NAME:" + text
@@ -59,6 +49,27 @@ class Parser():
         data = req.content.decode()  # Декодируем его ответ, чтобы Кириллица отображалась корректно
         req.close()
         return data
+
+    async def fetch(self, session, url):
+        async with session.get(url) as response:
+            if response.status != 200:
+                response.raise_for_status()
+            return await response.text()
+
+    async def fetch_all(self, session, urls):
+        tasks = []
+        for url in urls:
+            task = asyncio.create_task(self.fetch(session, url))
+            tasks.append(task)
+        results = await asyncio.gather(*tasks)
+        return results
+
+    async def get(self, urls, loop):
+        l = []
+        async with aiohttp.ClientSession(loop=loop) as session:
+            htmls = await self.fetch_all(session, urls)
+        l.append(htmls)
+        return l
 
     def process_key_skills(self, skills):
         return ", ".join([skill["name"] for skill in skills])
@@ -79,29 +90,49 @@ class Parser():
                  "description": strip_tags(row["description"]), "address_lat": lat,
                  "address_lng": lng}, index=[0]))
 
-    def df_to_csv(self, df, name):
+    def make_save_df(self, pages, name):
+        df = self.make_df(pages)
         df.to_csv(name, index=False)
 
     def getPages(self, n_pages):
-        q = queue.Queue()
+        urls = []
         for i in range(n_pages):
-            q.put(self.getPage(i))
-        return q
+            data = json.loads((self.getPage(i)))
+            urls = urls + [elem["url"] for elem in data["items"]]
+        return urls
 
-    def parse(self, pages, df):
-        for page in range(pages):
-            before = time.time()
-            jsObj = json.loads(parser.getPage(page))
-            print(time.time() - before)
-            for jsonbj in tqdm.tqdm(jsObj["items"]):
-                req = requests.get(jsonbj["url"])
-                data = req.content.decode()
-                req.close()
-                data = json.loads(data)
-                df = self.append_to_df(df, data)
-        self.df_to_csv(df, "data.csv")
+    def Parse(self, pages):
+        urls = self.getPages(pages)
+        loop = asyncio.get_event_loop()
+        htmls = loop.run_until_complete(self.get(urls, loop))
+        loop.close()
+        htmls = [json.loads(elem) for elem in htmls[0]]
+        return htmls
+
+    def make_df(self, pages):
+        df = pd.DataFrame(columns=['id', 'name', 'skills', 'desc', 'lat', 'lng'],
+                          index=range(pages * self.per_page))
+        rows = self.Parse(pages)
+        for i, row in enumerate(rows):
+            try:
+                lat = row["address"]["lat"]
+            except TypeError:
+                lat = 0
+            try:
+                lng = row["address"]["lng"]
+            except TypeError:
+                lng = 0
+            df.iloc[i]["id"] = row["id"]
+            df.iloc[i]["name"] = row["name"]
+            df.iloc[i]["skills"] = self.process_key_skills(row["key_skills"])
+            df.iloc[i]["desc"] = strip_tags(row["description"])
+            df.iloc[i]["lat"] = lat
+            df.iloc[i]["lng"] = lng
+        return df
 
 
-parser = Parser("Аналитик", 1, 10)
-data = pd.DataFrame()
-parser.parse(3, data)
+parser = Parser("Аналитик", 1, 20)
+time_before = time.time()
+parser.make_save_df(1, 'data.csv')
+time_after = time.time()
+print(time_after - time_before)
